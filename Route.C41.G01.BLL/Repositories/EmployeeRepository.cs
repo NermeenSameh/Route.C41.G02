796f8a5e5a16ed020efeb1deb1a243dd228cb390
NermeenSameh <nermeensameh1998@gmail.com>
2024-03-23 16:31:36 +0200

    Employee Repository - BLL

---
+++ Route.C41.G01.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route.C41.G01.BLL.Interfaces;
+using Route.C41.G02.DAL.Data;
+using Route.C41.G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41.G01.BLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly ApplicationDbContext _dbContext; // NULL
+
+        public EmployeeRepository(ApplicationDbContext dbContext) // Ask ClR For Creating Object From
+        {
+            //_dbContext = new ApplicationDbContext(new Microsoft.EntityFrameworkCore.DbContextOptions<ApplicationDbContext>());
+            _dbContext = dbContext;
+        }
+
+        public int Add(Employee entity)
+        {
+            _dbContext.Employees.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Update(Employee entity)
+        {
+            _dbContext.Employees.Update(entity);
+            return _dbContext.SaveChanges();
+
+        }
+
+        public int Delete(Employee entity)
+        {
+            _dbContext.Employees.Remove(entity);
+            return _dbContext.SaveChanges();
+
+        }
+
+        public IEnumerable<Employee> GetAll()
+          => _dbContext.Employees.AsNoTracking().ToList();
+
+        public Employee Get(int id)
+        {
+          
+            return _dbContext.Find<Employee>(id); // EF Core 3.1 New Feature 
+
+        }
+    }
+}
