27ca80fdb26802af503710e3218ab19ba0abdf06
NermeenSameh <nermeensameh1998@gmail.com>
2024-03-27 02:12:46 +0200

    AddSingletone Vs AddScoped vs AddTransient

--- Route.C41.G02.PL/Startup.cs
+++ Route.C41.G02.PL/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Hosting;
 using Route.C41.G01.BLL.Interfaces;
 using Route.C41.G01.BLL.Repositories;
 using Route.C41.G02.DAL.Data;
+using Route.C41.G02.PL.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -57,9 +58,12 @@ namespace Route.C41.G02.PL
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
-            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            // ApplicationServicesExtientions.AddApplicationServices(services); // Static Method
 
-            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            services.AddApplicationServices();  // Extension Method
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
