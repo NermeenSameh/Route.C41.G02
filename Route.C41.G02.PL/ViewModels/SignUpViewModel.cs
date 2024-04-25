e507661a5e247cd56c99774926a4e72cbfb02207
NermeenSameh <nermeensameh1998@gmail.com>
2024-04-25 22:01:46 +0200

    Draft

--- Route.C41.G02.PL/ViewModels/SignUpViewModel.cs
+++ Route.C41.G02.PL/ViewModels/SignUpViewModel.cs
@@ -21,13 +21,13 @@ namespace Route.C41.G02.PL.ViewModels
 
 
 
-        [Required(ErrorMessage = "Password id required")]
+        [Required(ErrorMessage = "Password is required")]
         [MinLength(5, ErrorMessage = "Minmum Password Lenght is 5")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
 
-        [Required(ErrorMessage = "Password id required")]
+        [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
         [Compare(nameof(Password), ErrorMessage = "Confirm Password doesn't match with Password")]
         public string ConfirmPassword { get; set; }
